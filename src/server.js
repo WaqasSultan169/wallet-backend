c63a00d0d24eb9868d64b73a16bd1434a8f91a15
MacBook Pro <macbookpro@192.168.1.68>
2025-07-10 15:58:43 +0500

    Fix:

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
-import job from "./config/cron.js";
 
 dotenv.config();
 
